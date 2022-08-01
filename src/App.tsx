ef0813c66ab9d89cbc9d7d283e3a7251226f6d54
0xknon <knyuwork@gmail.com>
2022-08-01 11:31:05 +0800

    feat: add workflow

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changes applied
         </a>
       </header>
     </div>
